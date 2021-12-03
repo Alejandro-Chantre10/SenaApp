5ec57e98591c172e7023846e663398161ee75c2e
Alejandro <chantre02alejo@gmail.com>
2021-12-03 15:39:58 -0500

    Se crea y nombra cada controlador, modelo y migracion del proyecto

---
+++ app/Http/Controllers/ControlApprenticeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ControlApprentice;
+use Illuminate\Http\Request;
+
+class ControlApprenticeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ControlApprentice  $controlApprentice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ControlApprentice $controlApprentice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ControlApprentice  $controlApprentice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ControlApprentice $controlApprentice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ControlApprentice  $controlApprentice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ControlApprentice $controlApprentice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ControlApprentice  $controlApprentice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ControlApprentice $controlApprentice)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/MedicalDataController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MedicalData;
+use Illuminate\Http\Request;
+
+class MedicalDataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MedicalData  $medicalData
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MedicalData $medicalData)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MedicalData  $medicalData
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MedicalData $medicalData)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MedicalData  $medicalData
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MedicalData $medicalData)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MedicalData  $medicalData
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MedicalData $medicalData)
+    {
+        //
+    }
+}
